b8fd3d8dca63842ae71b9f5c24a0c228588dc5c5
Henrique Oliveira <haooliveira@gmail.com>
2021-10-01 11:23:32 -0300

    Update terragrunt.rb

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -2,7 +2,7 @@ class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
   url "https://github.com/gruntwork-io/terragrunt/archive/v0.33.2.tar.gz"
-  sha256 "c10944f0633cc8c63380661c71da989a6d38c09ea3c183fd216742ca51f4e964"
+  sha256 "a740442f409268b5a6faad5324cb70fde10da0c4e2b408e1baea6e936feb40a5"
   license "MIT"
 
   bottle do
